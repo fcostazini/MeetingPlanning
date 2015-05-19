207fb71c165a4afcd77512763c4bed691b1e7a0e
unknown <cpastorino@nb-cpastorino1.conexia.com.ar>
2015-05-19 00:12:14 -0300

    prototype y evento a medias

--- web/js/Scripts MeetingPlanning.js
+++ web/js/Scripts MeetingPlanning.js
@@ -1,23 +1,27 @@
 
-function arrayContains(array, valor){
-	return array.indexOf(valor)>=0;
+function arrayContains(array, valor) {
+	return array.indexOf(valor) > =0;
 }
 
-function TotalCategoria(gastoCategoria, cantidadPersonas){
+
+
+function TotalCategoria(gastoCategoria, cantidadPersonas) {
 	this.gastoCategoria = gastoCategoria;
 	this.cantidadPersonas = cantidadPersonas;
-	this.getNombre = function(){ return this.gastoCategoria.nombre; };
-	this.getGasto = function(){ return this.gastoCategoria.gasto; };
-	this.agregarGasto = function(valor){ this.gastoCategoria.agregarGasto(valor)}
-	
 }
-
+	TotalCategoria.prototype.getNombre = function() { return this.gastoCategoria.nombre; };
+	TotalCategoria.prototype.getGasto = function() { return this.gastoCategoria.gasto; };
+	TotalCategoria.prototype.agregarGasto = function(valor) { this.gastoCategoria.agregarGasto(valor)};
+	
+	
+	
 function GastoCategoria(nombre,gasto){
 	this.nombre = nombre;
 	this.gasto = gasto;
-	this.agregarGasto = function(valor){this.gasto += valor;}
-	this.equals = function(valor){return this.nombre==valor.nombre;}
 }
+	GastoCategoria.prototype.agregarGasto = function(valor){this.gasto += valor;};
+	GastoCategoria.prototype.equals = function(valor){return this.nombre==valor.nombre;};
+
 
 
 function Invitado(gestorCategoria, nombre){
@@ -25,15 +29,16 @@ function Invitado(gestorCategoria, nombre){
 	this.gastosCategoria = [];
 	this.gestorCategoria = gestorCategoria;
 	this.nombre = nombre;
-	
-	this.agregarANoConsumir = function(nombreCategoria){
+}	
+
+	Invitado.prototype.agregarANoConsumir = function(nombreCategoria){
 		if(!arrayContains(this.categoriasNoConsumidas, nombreCategoria)){
 			this.categoriasNoConsumidas[this.categoriasNoConsumidas.length] = nombreCategoria;
 		}
 		//Agregar llamado al gestor para restar uno de la categoria
 	}
 
-	this.quitarDeNoConsumir = function(nombreCategoria){
+	Invitado.prototype.quitarDeNoConsumir = function(nombreCategoria){
 			
 			var  posicion = this.categoriasNoConsumidas.indexOf(nombreCategoria);
 			if(posicion>=0){
@@ -42,7 +47,7 @@ function Invitado(gestorCategoria, nombre){
 		//Agregar llamado al gestor para sumar uno de la categoria
 	}
 	
-	this.agregarGasto = function(gastoCategoria){
+	Invitado.prototype.agregarGasto = function(gastoCategoria){
 		for(var i in this.gastosCategoria){
 			if(gastoCategoria.equals(this.gastosCategoria[i])){
 				return false;
@@ -51,7 +56,7 @@ function Invitado(gestorCategoria, nombre){
 		this.gastosCategoria[this.gastosCategoria.length] = gastoCategoria;
 	}
 	
-	this.quitarGasto = function(gastoCategoria){
+	Invitado.prototype.quitarGasto = function(gastoCategoria){
 		var posicion = -1;
 		for(var i in this.gastosCategoria){
 			if(gastoCategoria.equals(this.gastosCategoria[i])){
@@ -66,6 +71,54 @@ function Invitado(gestorCategoria, nombre){
 		}
 	}
 	
+
+	
+function GestorCategoria(){
+	this.totalesCategoria = [];
+	
+}	
+
+
+
+function Evento(){
+	this.invitados = [];
+	this.gestorCategoria = new GestorCategoria();
 }
+	
+	Evento.prototype.abrirEvento(){
+		
+	}
+	
+	Evento.prototype.cerrarEvento(){
+		
+	}
+	
+	Evento.prototype.agregarInvitado(nombre){
+		for(var i in this.invitado){
+			if(nombre==this.invitados[i].nombre)){
+				return false;
+			}
+		}
+		this.invitados[this.invitados.length] = new Invitado(this.gestorCategoria, nombre);
+		//Falta llamar al gestor y sumar 1 a todas las categorias!
+	}
+	
+	Evento.prototype.quitarInvitado(nombre){
+		var posicion = -1;
+		for(var i in this.invitado){
+			if(nombre==this.invitados[i].nombre)){
+				posicion=i;
+				break;
+			}
+		}
+		if(posicion<0){
+			return false;
+		} else {
+			this.invitados.splice(posicion,1);			
+		}
+		//Falta llamar al gestor para:
+			//Restar 1 de todas las categorias que consuma el invitado.
+			//Restar los gastos de cada categoria que tenga el invitado.
+	}
 
 
