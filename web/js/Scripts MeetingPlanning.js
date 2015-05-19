f04c31d6501c12eff95f44b970040ffbb6a05506
FaQ <fcostazini@gmail.com>
2015-05-18 21:40:39 -0300

    Agrego Index HTML

--- web/js/Scripts MeetingPlanning.js
+++ web/js/Scripts MeetingPlanning.js
@@ -67,41 +67,5 @@ function Invitado(gestorCategoria, nombre){
 	}
 	
 }
-var TEST = {};
 
-TEST.Invitados = {
-	testAgregarGasto : function(){
-		var inv = new Invitado("gestor","Facundo");
-		inv.agregarGasto(new GastoCategoria("Carne", 20));
-		return (inv.gastosCategoria[0].nombre == "Carne" && inv.gastosCategoria[0].gasto == 20 );
-	},
-
-	testAgregarGastoYaExistente : function(){
-		var inv = new Invitado("gestor","Facundo");
-		inv.gastosCategoria[0] = new GastoCategoria("Carne", 20);
-		return !inv.agregarGasto(new GastoCategoria("Carne", 50));
-	},
-
-	testQuitarGasto : function(){
-		var inv = new Invitado("gestor","Facundo");
-		var gasto = new GastoCategoria("Carne", 20);
-		inv.gastosCategoria[0] = gasto;
-		inv.quitarGasto(new GastoCategoria("Carne", 20));
-		return (inv.gastosCategoria.indexOf(gasto) == -1);
-	},
-
-	testQuitarGastoNoExistente : function(){
-		var inv = new Invitado("gestor","Facundo");
-		var gasto = new GastoCategoria("Carne", 20);
-		inv.gastosCategoria[0] = gasto;
-		return !inv.quitarGasto(new GastoCategoria("Bebidas", 10));
-	},
-	run : function(){
-		for(var a in this){
-			if(a != "run")
-			console.log(a + ": " + this[a]());
-		}
-	}
-	
-}
 
